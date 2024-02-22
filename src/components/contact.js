4c73d4b46ea694b4b9ae8ffd900dad9d697300bd
Hussein Aligabi <aligabih@gmail.com>
2024-02-22 13:14:21 -0500

    First commit

---
+++ src/components/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Form</h2>
+      <p>Fill out the form below to contact us.</p>
+      {/* Add your contact form components here */}
+    </div>
+  );
+}
+
+export default Contact;
